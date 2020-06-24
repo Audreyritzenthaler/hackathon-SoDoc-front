4db246b95086204dd53b2c3ee60e3478b86f3290
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-06-24 21:38:46 +0200

    implem react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
+import LogBook from './components/patients/LogBook';
+import Dashboard from './components/practicioner/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <p>Sodoc</p>
+      <h1>Sodoc</h1>
+      <Switch>
+        <Route exact path="/" component={Connection}/>
+        <Route path="/logbook" component={LogBook}/>
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
     </div>
   );
 }
