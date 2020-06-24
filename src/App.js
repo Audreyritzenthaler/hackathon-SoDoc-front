5cb89885c1d2f52cc7dfee8c8ca5bc39d38a82f8
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-06-24 21:51:17 +0200

    comment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 import './App.css';
 import LogBook from './components/patients/LogBook';
-import Dashboard from './components/practicioner/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <h1>Sodoc</h1>
       <Switch>
-        <Route exact path="/" component={Connection}/>
+        {/* <Route exact path="/" component={Connection}/> */}
         <Route path="/logbook" component={LogBook}/>
-        <Route path="/dashboard" component={Dashboard} />
+        {/* <Route path="/dashboard" component={Dashboard} /> */}
       </Switch>
+
+      <Link to="/logbook">Je suis patient</Link>
+      {/* <Link to="/dashboard">Je suis doctor</Link> */}
     </div>
   );
 }
