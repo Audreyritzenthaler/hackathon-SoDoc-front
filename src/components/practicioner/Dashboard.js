b3f93adc2466b55249aa9c055becd29cc64f940f
Lola <lola.donval@gmail.com>
2020-06-25 17:03:18 +0200

    fix css

--- src/components/practicioner/Dashboard.js
+++ src/components/practicioner/Dashboard.js
@@ -9,7 +9,7 @@ const Dashboard = () => {
     <div className='Dashboard'>
       <BurgerMenuPractitioner />
       <div className='dash-info-container'>
-        <h3>Mes patients</h3>
+        <h3 className='dash-title'>Mes patients</h3>
         <div className='dash-input-container'>
           <input type='text' className='dash-input' />
           {/* <button className='dash-button'>Search</button> */}
