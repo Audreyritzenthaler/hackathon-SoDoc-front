5d23ecda19d03f75e218b341e1c5d835f10524da
Lola <lola.donval@gmail.com>
2020-06-26 05:14:20 +0200

    responsive css

--- src/components/patients/DosagePatient.js
+++ src/components/patients/DosagePatient.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
+import logo from '../../logo.png'
+import BurgerMenu from './BurgerMenuPatient'
 import '../Dosage.css'
 
 class DosagePatient extends Component {
   render() {
     return (
-      <div className='Dosage'>
-        <h3>My dosages</h3>
-        <div className='dosage-details'>
-          <p>Nom du médicament</p>
-          <p>Posologie</p>
-          <p>Durée du traitement</p>
+      <div>
+        <div className='nav-responsive'>
+          <img src={logo} alt='logo of feelback' style={{ marginLeft: '1rem', marginTop: '1rem', width: '4rem' }} />
+          <BurgerMenu />
+        </div>
+        <div className='Dosage'>
+          <h3>My dosages</h3>
+          <div className='dosage-details'>
+            <p>Nom du médicament</p>
+            <p>Posologie</p>
+            <p>Durée du traitement</p>
+          </div>
         </div>
       </div>
     )
